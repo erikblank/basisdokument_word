b07da79c6174ab65663b8bd33576497c18fa58fd
erikblank <erikblank97@web.de>
2023-08-24 12:32:02 +0200

    first version of treeitems for section and entries with real data

--- src/taskpane/components/treeView/StyledTreeItem.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const StyledTreeItem = () => {
-  return <div>aasd</div>;
-};
-
-export default StyledTreeItem;
--- src/taskpane/pages/Main.tsx
+++ src/taskpane/pages/Main.tsx
@@ -1,13 +1,59 @@
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import { TreeItem, TreeView } from "@mui/lab";
+import { TreeView } from "@mui/lab";
+import { ThemeProvider } from "@mui/material";
 import React from "react";
+import EntryTreeItem from "../components/treeView/EntryTreeItem";
+import SectionTreeItem from "../components/treeView/SectionTreeItem";
 import { useCase, useSection } from "../contexts";
+import { theme } from "../theme/muiTheme";
 
 /* global console */
 
+/* const section: ISection = {
+  id: "80456c3c-ab0d-453a-b92f-57020acd6d5b",
+  num: 1,
+  titleDefendant: "Gliederungspunkt defendant",
+  titlePlaintiff: "Gliederung punkt 2 klage",
+  titlePlaintiffVersion: 1,
+  version: 0,
+}; */
+
+/* const entries: IEntry[] = [
+  {
+    id: "111",
+    entryCode: "1-1-1",
+    version: 1,
+    text: "asdf",
+    author: "a a",
+    role: UserRole.Defendant,
+    sectionId: "1",
+    evidences: [],
+  },
+  {
+    id: "222",
+    entryCode: "1-1-2",
+    version: 1,
+    text: "asdf",
+    author: "a a",
+    role: UserRole.Plaintiff,
+    sectionId: "1",
+    evidences: [],
+  },
+  {
+    id: "333",
+    entryCode: "1-1-3",
+    version: 1,
+    text: "asdf",
+    author: "a a",
+    role: UserRole.Defendant,
+    sectionId: "1",
+    evidences: [],
+    associatedEntry: "1-1-2",
+  },
+]; */
+
 const Main = () => {
-  /* const { user } = useUser(); */
   const { entries } = useCase();
   const { sectionList } = useSection();
 
@@ -17,21 +63,24 @@ const Main = () => {
   return (
     <>
       {/* <Sidebar /> */}
-      <TreeView
-        defaultCollapseIcon={<ExpandMoreIcon />}
-        defaultExpandIcon={<ChevronRightIcon />}
-        sx={{ flexGrow: 1, overflowY: "auto" }}
-      >
-        {sectionList.map((section) => (
-          <TreeItem key={section.id} nodeId={section.id} label={section.titlePlaintiff}>
-            {entries
-              .filter((entry) => entry.sectionId === section.id)
-              .map((filteredEntry) => (
-                <TreeItem key={filteredEntry.id} nodeId={filteredEntry.id} label={filteredEntry.entryCode} />
-              ))}
-          </TreeItem>
-        ))}
-      </TreeView>
+      <ThemeProvider theme={theme}>
+        <TreeView
+          defaultCollapseIcon={<ExpandMoreIcon />}
+          defaultExpandIcon={<ChevronRightIcon />}
+          sx={{ flexGrow: 1, overflowY: "auto" }}
+        >
+          {sectionList.map((section) => {
+            const filteredEntries = entries.filter((entry) => entry.sectionId === section.id);
+            return (
+              <SectionTreeItem key={section.id} nodeId={section.id} section={section}>
+                {filteredEntries.map((entry) => (
+                  <EntryTreeItem key={entry.id} nodeId={entry.id} entry={entry} />
+                ))}
+              </SectionTreeItem>
+            );
+          })}
+        </TreeView>
+      </ThemeProvider>
     </>
   );
 };
